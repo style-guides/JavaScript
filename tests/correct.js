effb85cb27ab5bbd7366b2b4c4d814e04472f6d5
Adrian Sieber <mail@adriansieber.com>
2016-07-27 19:27:58 +0000

    Add tests

---
+++ tests/correct.js
@@ -0,0 +1,15 @@
+const nativeConsole = require('console')
+const log = new nativeConsole.Console(process.stdout, process.stderr)
+let status = ''
+
+// Incorrect placement of ternary operator
+const isTest = true
+status = isTest
+  ? 'is a test'
+  : 'is not a test'
+
+// Incorrect placement of operators
+status = 'This is a' +
+  'test'
+
+log.info(status)
